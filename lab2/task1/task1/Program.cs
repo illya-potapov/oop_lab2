e740a8c033145a2704abb64b0ef1a634ac01b6b6
Ilya Potapov <illya.potapov88@gmail.com>
2020-09-17 13:51:49 +0300

    thanks

---
+++ lab2/task1/task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool one = true;
+            byte bit1 = 10;
+            sbyte bit2 = -10;
+            short sh1 = -30000;
+            ushort sh2 = 30000;
+            int a = 1000000;
+            uint b = 4000000000;
+            long c1 = -9;
+            ulong c2 = 9;
+            float d = 3.1F;
+            decimal d1 = 3.4M;
+            double e = 3.1;
+            char f = 'A';
+            string str = "Hello";
+            object obj = 3.13;
+
+
+            Console.WriteLine($"Bool: {one}");
+            Console.WriteLine($"Byte: {bit1}");
+            Console.WriteLine($"sByte: {bit2}");
+            Console.WriteLine($"Short: {sh1}");
+            Console.WriteLine($"uShort: {sh2}");
+            Console.WriteLine($"Int: {a}");
+            Console.WriteLine($"Uint: {b}");
+            Console.WriteLine($"Long: {c1}");
+            Console.WriteLine($"uLong: {c2}");
+            Console.WriteLine($"Float: {d}");
+            Console.WriteLine($"Decimal: {d1}");
+            Console.WriteLine($"Double: {e}");
+            Console.WriteLine($"Char: {f}");
+            Console.WriteLine($"String: {str}");
+            Console.WriteLine($"Object: {obj}");
+
+        }
+    }
